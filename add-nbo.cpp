648d6714436be12bc423d5a5c145a84208af8b84
Juuuho <gjwngh02@gmail.com>
2022-07-26 06:20:30 +0000

    Real Final

--- add-nbo.cpp
+++ add-nbo.cpp
@@ -11,13 +11,13 @@ int main(int argc, char* argv[]) {
 
 	FILE* fp1 = fopen(argv[1], "rb");
 	if(fp1==NULL){
-		printf("An Error occured while opening <file1>");
+		printf("An Error occured while opening <file1>\n");
 		return 1;
 	}
 
 	FILE* fp2 = fopen(argv[2], "rb");
 	if(fp2==NULL){
-		printf("An Error occured while opening <file2>");
+		printf("An Error occured while opening <file2>\n");
 		return 1;
 	}
 
