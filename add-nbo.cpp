bb984c71d0859fbc5aa5889f2b314e99b903ee89
Juuuho <gjwngh02@gmail.com>
2022-07-26 06:17:20 +0000

    Final

--- add-nbo.cpp
+++ add-nbo.cpp
@@ -1,9 +1,9 @@
 #include <stdio.h>
 #include <stdint.h>
+#include <netinet/in.h>
 
 int main(int argc, char* argv[]) {
-	uint32_t file1, file2;
-
+	uint32_t n1, n2, sum;
 	
 	if (argc != 3){
 		printf("usage: add-nbo.o <file1> <file2>\n");
@@ -21,6 +21,15 @@ int main(int argc, char* argv[]) {
 		return 1;
 	}
 
+	fread(&n1, sizeof(uint32_t), sizeof(n1), fp1);
+	fread(&n2, sizeof(uint32_t), sizeof(n2), fp2);
+
+	n1 = ntohl(n1);
+	n2 = ntohl(n2);
+	
+	sum = n1 + n2;
+
+	printf("%d(0x%x) + %d(0x%x) = %d(0x%x)\n", n1, n1, n2, n2, sum, sum);
 
 }
 
