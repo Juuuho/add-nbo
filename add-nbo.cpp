0f9c2e0d6084618ea8ec8519966c7983d903306d
Juuuho <gjwngh02@gmail.com>
2022-07-26 06:01:43 +0000

    change add-nbo

--- add-nbo.cpp
+++ add-nbo.cpp
@@ -1,7 +1,26 @@
 #include <stdio.h>
 #include <stdint.h>
 
-int main() {
-	printf("ggg");
+int main(int argc, char* argv[]) {
+	uint32_t file1, file2;
+
+	
+	if (argc != 3){
+		printf("usage: add-nbo.o <file1> <file2>\n");
+	}
+
+	FILE* fp1 = fopen(argv[1], "rb");
+	if(fp1==NULL){
+		printf("An Error occured while opening <file1>");
+		return 1;
+	}
+
+	FILE* fp2 = fopen(argv[2], "rb");
+	if(fp2==NULL){
+		printf("An Error occured while opening <file2>");
+		return 1;
+	}
+
+
 }
 
